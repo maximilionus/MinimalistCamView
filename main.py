cb15438677e00245d8fd655b4b7cc98f94896837
maximilionus <xxxarakxxx@gmail.com>
2020-06-06 20:31:20 +0300

    Remaking the code

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-import tkinter
+import numpy as np
 from cv2 import cv2
+import tkinter as tk
+from PIL import Image, ImageTk
 
-cap = cv2.VideoCapture(0)
+# Set up GUI
+window = tk.Tk()  # Makes main window
+window.wm_title("Digital Microscope")
+window.config(background="#FFFFFF")
 
-while(True):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
-   
-	# Display the resulting frame
-	cv2.imshow('frame',frame)
-	if cv2.waitKey(20) & 0xFF == ord('q'):
-		break
+# Graphics window
+imageFrame = tk.Frame(window, width=600, height=500)
+imageFrame.grid(row=0, column=0, padx=10, pady=2)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+# Capture video frames
+lmain = tk.Label(imageFrame)
+lmain.grid(row=0, column=0)
+cap = cv2.VideoCapture('rtsp://ip') # TODO: Magic
+
+
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_frame)
+
+
+# Slider window (slider controls stage position)
+sliderFrame = tk.Frame(window, width=600, height=100)
+sliderFrame.grid(row=600, column=0, padx=10, pady=2)
+
+
+show_frame()  # Display 2
+window.mainloop()  # Starts GUI
