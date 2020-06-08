0cffbdd4b8dd2fc1bfe4910ed04dbcbfba1326e2
maximilionus <xxxarakxxx@gmail.com>
2020-06-08 18:12:58 +0300

    Themes implemented - Overall UI upgrade - Added Camlist menu (wip)

--- MinimalistCamView/main.py
+++ MinimalistCamView/main.py
@@ -1,5 +1,7 @@
 from cv2 import cv2
 import tkinter as tk
+from tkinter import ttk
+from ttkthemes import ThemedStyle
 from PIL import Image, ImageTk
 from threading import Thread
 import logging
@@ -13,8 +15,10 @@ class MCV_UI(tk.Tk):
         super().__init__()
         h.MCVConfig.create()
         self.__logger = logging.getLogger('MCV_UI')
+        self.__ttk_style = ThemedStyle(self)
+        self.__ttk_style.set_theme('equilux')
         self.__showUI__cam_view()
-        self.__logger.info('User Interface is ready')
+        self.__logger.info('User Interface is ready.')
 
         self.__pull_frame_loop_enabled = False
 
@@ -36,25 +40,26 @@ class MCV_UI(tk.Tk):
                 self.__logger.info('End frame pulling.')
 
         self.columnconfigure(0, weight=1)
-        self.__frame_top = tk.Frame(self, bg="#181818")
-        self.__button_play_switch = tk.Button(self.__frame_top, text=h.U_SYMBOLS['play'], width=20, command=cam_playswitch, bg="#404040", fg="#ffffff", relief=tk.FLAT)
+        self.__frame_top = ttk.Frame(self)
+        self.__button_play_switch = ttk.Button(self.__frame_top, text=h.U_SYMBOLS['play'], width=20, command=cam_playswitch)
         self.__button_play_switch.grid(row=0, column=0, sticky="W")
-        self.__button_cams = tk.Button(self.__frame_top, text=h.U_SYMBOLS['vertical_dots'], command=self.__showUI__cam_list, bg="#404040", fg="#ffffff", relief=tk.FLAT, width=10)
+        self.__button_cams = ttk.Button(self.__frame_top, text=h.U_SYMBOLS['vertical_dots'], command=self.__showUI__cam_list, width=10)
         self.__button_cams.grid(row=0, column=2, sticky="W")
         self.__frame_top.grid(row=0, column=0, sticky="NWE")
 
         # Bottom
         self.rowconfigure(1, weight=1)
-        self.__frame_bot = tk.Frame(self, bg="#181818")
+        self.__frame_bot = ttk.Frame(self)
         self.__frame_bot.rowconfigure(0, weight=1)
         self.__frame_bot.columnconfigure(0, weight=1)
         self.__frame_bot.grid(row=1, column=0, sticky="NSEW")
 
-        self.__label_cam = tk.Label(self.__frame_bot, bg='#181818')
+        self.__label_cam = ttk.Label(self.__frame_bot)
         self.__label_cam.grid(row=0, column=0, sticky='NSEW')
 
     def __cam_connect(self):
         self.__cam_capture = cv2.VideoCapture(temp_config.CONNECTION)  # TODO: Read from config
+        self.__logger.info("Successfully connected to cam.")
 
     def __pull_frame_loop(self):
         is_pulled, frame = self.__cam_capture.read()
@@ -66,27 +71,54 @@ class MCV_UI(tk.Tk):
             self.__label_cam.configure(image=imgtk)
 
         if self.__pull_frame_loop_enabled:
-            self.__label_cam.after(100, self.__pull_frame_loop)
+            self.__label_cam.after(10, self.__pull_frame_loop)
 
     def __showUI__cam_list(self):
         def update_cam_list():
             def ucl_thread():
                 cams = h.MCVConfig.get()["cam_list"].keys()
                 for cam in cams:
-                    listbox.insert(tk.END, cam)
+                    lb_cams.insert(tk.END, cam)
             Thread(target=ucl_thread).start()
+
+        def on_close():
+            root.destroy()
+            self.__button_cams.config(state=tk.NORMAL)
+
+        self.__button_cams.config(state=tk.DISABLED)
 
         root = tk.Toplevel(self)
         root.geometry('300x500')
         root.title('Cams')
-        frame_left = tk.Frame(root)
+        root.protocol("WM_DELETE_WINDOW", on_close)
+
+        # Left Frame
+        frame_left = ttk.Frame(root)
         frame_left.grid(row=0, column=0, sticky="NSEW")
-        listbox = tk.Listbox(frame_left, selectmode=tk.SINGLE, bg="#181818", fg="#ffffff")
-        listbox.grid(row=0, column=0, sticky="NSEW")
-        frame_left.rowconfigure(0, weight=1)
-        frame_left.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
         root.columnconfigure(0, weight=1)
+
+        lb_cams = tk.Listbox(frame_left, bg="#303030", selectmode=tk.SINGLE)
+        lb_cams.grid(row=0, column=0, sticky="NSEW")
+        lb_cams_scroll = ttk.Scrollbar(frame_left, command=lb_cams.yview)
+        lb_cams.config(yscrollcommand=lb_cams_scroll.set)
+        lb_cams_scroll.grid(row=0, column=1, sticky="NS")
+        frame_left.rowconfigure(0, weight=1)
+        frame_left.columnconfigure(0, weight=1)
+
+        # Right Frame
+        frame_right = ttk.Frame(root)
+        frame_right.grid(row=0, column=1, sticky="NSEW")
+        root.columnconfigure(1, weight=1)
+
+        button_use = ttk.Button(frame_right, text="Use")
+        button_edit = ttk.Button(frame_right, text="Edit")
+        button_add = ttk.Button(frame_right, text="Add")
+        button_use.grid(row=0, column=0, sticky="NEW")
+        button_edit.grid(row=1, column=0, sticky="NEW")
+        button_add.grid(row=2, column=0, sticky="NEW")
+        frame_right.columnconfigure(0, weight=1)
+
         update_cam_list()
 
 
